92702000a4aa43a8fa65cf8bf396ba1696cd1e40
tejashshah3 <tejashshah3@988a16fe-9e4a-0410-a620-0db16a4de7c0>
2008-04-26 06:05:20 +0000

    a

---
+++ trunk/wfdwebapp/src/java/travel/Trip.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package travel;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author tejash
+ */
+public class Trip implements Serializable {
+    public Trip() {
+        
+    }
+
+}
