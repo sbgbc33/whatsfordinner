705274d0021e5e9d6cdb2e9a3c5def73913aaf00
tejashshah3 <tejashshah3@988a16fe-9e4a-0410-a620-0db16a4de7c0>
2008-04-26 06:06:34 +0000

    a

---
+++ trunk/wfdmodel/src/org/wfd/model/House.java
@@ -0,0 +1,35 @@
+package org.wfd.model;
+
+
+// <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+// #[regen=yes,id=DCE.02F98068-A038-E6C4-12D5-6E6ABAD9547B]
+// </editor-fold> 
+public class House {
+
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+    // #[regen=yes,id=DCE.CBAD9B1B-AA68-22DB-598F-EE1AD72331DC]
+    // </editor-fold> 
+    private String name;
+
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+    // #[regen=yes,id=DCE.84AEBB12-48EA-4BDC-90AA-3A683C73D6C3]
+    // </editor-fold> 
+    public House () {
+    }
+
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+    // #[regen=yes,regenBody=yes,id=DCE.A72A48DB-509F-DAA6-8F43-29DE443F66FF]
+    // </editor-fold> 
+    public String getName () {
+        return name;
+    }
+
+    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
+    // #[regen=yes,regenBody=yes,id=DCE.2C31245B-152B-FE06-61F9-8A29D674544E]
+    // </editor-fold> 
+    public void setName (String val) {
+        this.name = val;
+    }
+
+}
+
