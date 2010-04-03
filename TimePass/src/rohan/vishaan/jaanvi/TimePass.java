8c3baf5c29905b01e13e96d6e066857b3f480c8a
tejashshah3 <tejashshah3@988a16fe-9e4a-0410-a620-0db16a4de7c0>
2010-04-03 03:28:21 +0000

    first working version.

--- TimePass/src/rohan/vishaan/jaanvi/TimePass.java
+++ TimePass/src/rohan/vishaan/jaanvi/TimePass.java
@@ -22,8 +22,10 @@ import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.AttributeSet;
 import android.util.DisplayMetrics;
 import android.view.View;
 import android.view.ViewGroup;
@@ -38,6 +40,9 @@ import android.widget.ImageView;
  */
 public class TimePass extends Activity {
 
+	private static int columnWidth;
+	private static int columnHeight;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -45,14 +50,17 @@ public class TimePass extends Activity {
 		setContentView(R.layout.grid_2);
 
 		GridView g = (GridView) findViewById(R.id.myGrid);
+		g.setBackgroundColor(Color.GRAY);
 
 		DisplayMetrics dm = new DisplayMetrics();
 		getWindowManager().getDefaultDisplay().getMetrics(dm);
 		int screenWidth = dm.widthPixels;
 		int screenHeight = dm.heightPixels;
 
-		int columnWidth = screenWidth / 2;
-		int columnHeight = screenHeight / 2;
+		columnWidth = screenWidth / 3;
+		columnHeight = screenHeight / 3;
+
+		g.setNumColumns(2);
 
 		g.setColumnWidth(columnWidth);
 		g.setAdapter(new ImageAdapter(this));
@@ -60,25 +68,54 @@ public class TimePass extends Activity {
 
 	public class MyImageView extends ImageView {
 
+		public MyImageView(Context context, AttributeSet attrs, int defStyle) {
+			super(context, attrs, defStyle);
+		}
+
+		public MyImageView(Context context, AttributeSet attrs) {
+			super(context, attrs);
+		}
+
 		public MyImageView(Context context) {
 			super(context);
 		}
 
-		private int imageId;
+		private int currentImageId;
+		private int origImageId;
 
-		public int getImageId() {
-			return imageId;
+		public void setOrigImageId(int i) {
+			this.origImageId = i;
 		}
 
-		@Override
-		public void setImageResource(int resId) {
-			this.imageId = resId;
+		public int getCurrentImageId() {
+			return currentImageId;
 		}
 
+		public int getOrigImageId() {
+			return origImageId;
+		}
+
+		private boolean matched;
+
+		public void setMatched() {
+			this.matched = true;
+			this.openCard();
+		}
+
+		public void openCard() {
+			super.setImageResource(this.getOrigImageId());
+		}
+
+		public void closeCard() {
+			super.setImageResource(R.drawable.sample_thumb_5);
+		}
 	}
 
 	public class ImageAdapter extends BaseAdapter implements OnClickListener {
 		private static final int DIALOG_YES_NO_MESSAGE = 1;
+		private int clickCount;
+
+		private int matchCount;
 
 		public ImageAdapter(Context c) {
 			mContext = c;
@@ -97,38 +134,76 @@ public class TimePass extends Activity {
 		}
 
 		public View getView(int position, View convertView, ViewGroup parent) {
-			ImageView imageView;
+			MyImageView imageView;
 			if (convertView == null) {
-				imageView = new ImageView(mContext);
-				imageView.setLayoutParams(new GridView.LayoutParams(45, 45));
+				imageView = new MyImageView(mContext);
+				imageView.setLayoutParams(new GridView.LayoutParams(
+						columnWidth, columnHeight));
 				imageView.setAdjustViewBounds(false);
 				imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-				imageView.setPadding(0, 0, 0, 0);
+				imageView.setPadding(0, 5, 0, 5);
 			} else {
-				imageView = (ImageView) convertView;
+				imageView = (MyImageView) convertView;
 			}
 
-			imageView.setImageResource(mThumbIds[position]);
+			imageView.setImageResource(R.drawable.sample_thumb_5);
+			imageView.setOrigImageId(mThumbIdsMatch[position]);
 
 			imageView.setOnClickListener(this);
 
 			return imageView;
 		}
 
+		public MyImageView lastClickOnImage;
+
 		public void onClick(View v) {
-			if (v instanceof ImageView) {
-				ImageView iv = (ImageView) v;
-				iv.setImageResource(mThumbIds[0]);
-				iv.refreshDrawableState();
+			if (v instanceof MyImageView) {
+				final MyImageView iv = (MyImageView) v;
+				if (iv.matched) {
+					return;// Ignore clicks on matched cards.
+				}
+				clickCount++;
+				if (clickCount % 2 == 1) {
+					// First Click
+					lastClickOnImage = iv;
+					iv.openCard();
+				} else {
+					// Second Click
+					if (iv.getOrigImageId() == lastClickOnImage
+							.getOrigImageId()) {
+						// Match found.
+						iv.setMatched();
+						lastClickOnImage.setMatched();
+						matchCount++;
+						if (matchCount == 2) {
+							showDialog(DIALOG_YES_NO_MESSAGE);
+						}
+					} else {
+						iv.openCard();
+						iv.postDelayed(new Runnable() {
+
+							public void run() {
+								lastClickOnImage.closeCard();
+								iv.closeCard();
+							}
+
+						}, 1000);
+					}
+				}
 			}
-			// showDialog(DIALOG_YES_NO_MESSAGE);
 		}
 
 		private final Context mContext;
 
-		private final Integer[] mThumbIds = { R.drawable.sample_thumb_0,
-				R.drawable.sample_thumb_1, R.drawable.sample_thumb_2,
-				R.drawable.sample_thumb_3 };
+		private final Integer[] mThumbIds = { R.drawable.sample_thumb_5,
+				R.drawable.sample_thumb_5, R.drawable.sample_thumb_5,
+				R.drawable.sample_thumb_5 };
+
+		private final Integer[] mThumbIdsMatch = { R.drawable.sample_thumb_0,
+				R.drawable.sample_thumb_1, R.drawable.sample_thumb_0,
+				R.drawable.sample_thumb_1 };
+
+		private final Integer hiddenCardId = R.drawable.sample_thumb_5;
 		// , R.drawable.sample_thumb_4,
 		// R.drawable.sample_thumb_5, R.drawable.sample_thumb_6,
 		// R.drawable.sample_thumb_7, R.drawable.sample_thumb_0,
